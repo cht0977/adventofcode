2d1c2aa06cf832b00fbb82487b1f7b7f28150c7a
cht0977 <cht0977@thi.de>
2022-12-10 22:49:21 +0100

    combine 9a and 9b

--- 2022/day9/runb.groovy
+++ 2022/day9/runb.groovy
@@ -1,56 +1,42 @@
 package day9
 
-final Map<String, Position> movements = [
-        'R': new Position(1, 0),
-        'L': new Position(-1, 0),
-        'U': new Position(0, 1),
-        'D': new Position(0, -1)
-]
+run(1)
+run(9)
 
-Set<Position> visitedCoordinates = [new Position(0, 0)]
-Position[] distancesBetweenKnots = [new Position(0, 0)] * 9
-def lastPositionOfEigthsKnot = new Position(0,0)
+void run(int length) {
 
-new File("input").collect {it as String}.collect {[it[0]]*(it.split(' ')[1] as int)}.each { allMovesFromLine ->
-    allMovesFromLine.each {moveInput ->
-        lastPositionOfEigthsKnot = doMoveForInput(moveInput, distancesBetweenKnots, lastPositionOfEigthsKnot, movements, visitedCoordinates)
-    }
-}
+    Set<Position> visitedCoordinates = [new Position(0, 0)]
+    Position[] distancesBetweenKnots = [new Position(0, 0)] * length
+    def currentTailPosition = new Position(0,0)
 
-println visitedCoordinates.size()
-println distancesBetweenKnots
+    new File("input")
+            .collect {it as String}
+            .collect {[it[0]]*(it.split(' ')[1] as int)}
+            .each { allMovesFromLine ->
+                allMovesFromLine.collect { instruction -> getMoveForInstruction(instruction) }.each {nextMove ->
+                    currentTailPosition = doMoveForInput(nextMove, distancesBetweenKnots, currentTailPosition, visitedCoordinates, length)
+                }
+            }
 
-int minX = visitedCoordinates*.x().min()
-int minY = visitedCoordinates*.y().min()
-int maxX = visitedCoordinates*.x().max()
-int maxY = visitedCoordinates*.y().max()
+    println visitedCoordinates.size()
+    println distancesBetweenKnots
+    drawResult(visitedCoordinates)
 
-for(y in maxY+2..minY-2) {
-    for (x in minX-2..maxX+2) {
-        if(x == 0 && y ==0) {
-            print "s"
-        }
-        else if(visitedCoordinates.contains(new Position(x, y))) {
-            print "#"
-        } else {
-            print "."
-        }
-    }
-    println()
 }
 
 
-Position doMoveForInput(String moveInput, Position[] distances, Position currentTailPosition, movements, visitedCoordinates) {
-    Position moveFromPreviousKnot = movements[moveInput]
-    for (i in 0..8) {
-        distances[i] = calculateDistanceToPreviousKnot(distances[i], moveFromPreviousKnot)
-        if(!currentKnotHasToMove(distances[i])) {
+Position doMoveForInput(Position moveInput, Position[] distances, Position currentTailPosition, visitedCoordinates, length) {
+    Position moveFromPreviousKnot = moveInput
+    for (i in 0..length-1) {
+        def newDistance = calculateDistanceToPreviousKnot(distances[i], moveFromPreviousKnot)
+        if(!currentKnotHasToMove(newDistance)) {
+            distances[i] = newDistance
             return currentTailPosition
         }
-        def newDistanceToPreviousKnot = calculateNewRelativePositionAfterMoving(distances[i])
-        def moveFromCurrentKnot = distances[i].diff(newDistanceToPreviousKnot)
+        def newDistanceToPreviousKnot = calculateNewRelativePositionAfterMoving(newDistance)
+        def moveFromCurrentKnot = newDistance.diff(newDistanceToPreviousKnot)
         distances[i] = newDistanceToPreviousKnot
-        if(i == 8) {
+        if(i == length-1) {
             currentTailPosition = calculateDistanceToPreviousKnot(currentTailPosition, moveFromCurrentKnot)
             visitedCoordinates.add(currentTailPosition)
             return currentTailPosition
@@ -73,3 +59,33 @@ boolean currentKnotHasToMove(Position relativePosition) {
     return Math.abs(relativePosition.x()) == 2 || Math.abs(relativePosition.y()) == 2
 }
 
+Position getMoveForInstruction(String instruction) {
+    final Map<String, Position> movements = [
+            'R': new Position(1, 0),
+            'L': new Position(-1, 0),
+            'U': new Position(0, 1),
+            'D': new Position(0, -1)
+    ]
+    return movements.get(instruction)
+}
+
+void drawResult(Set visitedCoordinates) {
+    int minX = visitedCoordinates*.x().min()
+    int minY = visitedCoordinates*.y().min()
+    int maxX = visitedCoordinates*.x().max()
+    int maxY = visitedCoordinates*.y().max()
+
+    for(y in maxY+2..minY-2) {
+        for (x in minX-2..maxX+2) {
+            if(x == 0 && y ==0) {
+                print "s"
+            }
+            else if(visitedCoordinates.contains(new Position(x, y))) {
+                print "#"
+            } else {
+                print "."
+            }
+        }
+        println()
+    }
+}
